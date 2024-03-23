baf39f76d6227ced74ebd3130cab4627f57aca58
WendyKariuki <phaminuswendy@gmail.com>
2024-03-23 20:17:29 +0300

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution in this file!
+let employee = {
+    name: "Mandy",
+    streetAddress: "Palm Beach 112"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+    }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee = [...employee];
+//     if (key in newEmployee) {
+//         delete newEmployee[key];
+//     }
+//     // delete newEmployee[key];
+//     return newEmployee;
+// }
+
+function deleteFromEmployeeByKey(employee, key) {
+        if (typeof employee !== 'object' || employee === null) {
+        console.error('Invalid employee object provided.');
+        return null;
+    }
+   const newEmployee = { ...employee };
+    
+    if (key in newEmployee) {
+        delete newEmployee[key];
+    }
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    if (typeof employee !== 'object' || employee === null) {
+        console.error('Invalid employee object provided.');
+        return null;
+    }
+    if (key in employee) {
+        delete employee[key];
+    } else {
+        console.error(`Key '${key}' does not exist in the employee object.`);
+    }
+       return employee;
+}
